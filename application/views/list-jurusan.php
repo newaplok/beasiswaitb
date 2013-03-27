9d05459fc1ccb1920ed3ca2cc6b7cf193c0277fe
Ananta Pandu <anpandumail@gmail.com>
2013-03-27 14:38:56 +0700

    delete semua

--- application/views/list-jurusan.php
+++
@@ -1,5 +0,0 @@
-<option value="IF">Teknik Informatika</option>
-<option value="II">Sekolah dan Teknologi Informasi</option>
-<option value="EL">Teknik Elektro</option>
-<option value="TE">Teknik Telekomunikasi</option>
-<option value="PO">Teknik Tenaga Listrik</option>
--- application/views/pesan_view.php
+++
@@ -1,4 +0,0 @@
-<h1>
-    <?php echo $pesan;?>
-</h1>
-<a href="<?php echo base_url();?>index.php/mahasiswa_ctrl/profil">kembali</a>
